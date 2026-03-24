7fa7d4872722e80e72fc9ff4aee83b694e7f7605
devjuju <jleleubellpro1994@gmail.com>
2026-03-24 11:48:56 +0100

    Codacy set up 3

--- app/assets/js/utils.js
+++ app/assets/js/utils.js
@@ -1,21 +1,40 @@
 window.smartScroll = (wrapper, element) => {
+    const isRow = window.getComputedStyle(wrapper).flexDirection === "row";
+
     element.scrollIntoView({
         behavior: "smooth",
-        inline: window.getComputedStyle(wrapper).flexDirection === "row"
-            ? "start"
-            : "nearest",
-        block: window.getComputedStyle(wrapper).flexDirection === "row"
-            ? "nearest"
-            : "start",
+        inline: isRow ? "start" : "nearest",
+        block: isRow ? "nearest" : "start",
     });
 };
 
 window.cloneTemplate = (id, index) => {
     const template = document.getElementById(id);
-    if (!template) return null;
 
-    const div = document.createElement("div");
-    div.innerHTML = template.innerHTML.replace(/__name__/g, index);
+    if (!template || !template.content) return null;
 
-    return div.firstElementChild;
+    const clone = template.content.cloneNode(true);
+
+    const replacePlaceholders = (node) => {
+        if (node.nodeType === Node.TEXT_NODE) {
+            node.textContent = node.textContent.replace(/__name__/g, index);
+        }
+
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            [...node.attributes].forEach(attr => {
+                if (attr.value.includes("__name__")) {
+                    node.setAttribute(
+                        attr.name,
+                        attr.value.replace(/__name__/g, index)
+                    );
+                }
+            });
+
+            node.childNodes.forEach(replacePlaceholders);
+        }
+    };
+
+    replacePlaceholders(clone);
+
+    return clone.firstElementChild;
 };
