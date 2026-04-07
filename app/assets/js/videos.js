5d8d3108577ce45dcad94a9285f8e1ca34c0f406
devjuju <jleleubellpro1994@gmail.com>
2026-04-07 18:55:28 +0200

    codacy corrections videos js 2

--- app/assets/js/videos.js
+++ app/assets/js/videos.js
@@ -36,7 +36,8 @@ document.addEventListener("DOMContentLoaded", () => {
         const removeBtn = item.querySelector(".remove-item");
 
         const updatePreview = () => {
-            const id = extractYoutubeId(input.value.trim());
+            const value = input.value.trim();
+            const id = extractYoutubeId(value);
 
             if (id) {
                 preview.src = `https://www.youtube.com/embed/${id}`;
